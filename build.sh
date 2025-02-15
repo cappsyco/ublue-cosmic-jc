6ea688f2805ffdf0ab74071ec47ecc413d538050
Jonathan Capps <cappsy@gmail.com>
2025-02-15 01:39:21 +0000

    adding cosmic desktop to base image

--- build.sh
+++ build.sh
@@ -9,10 +9,16 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# install cosmic
+curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo \
+            https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$(rpm -E %fedora)/ryanabx-cosmic-epoch-fedora-$(rpm -E %fedora).repo
+dnf5 install -y cosmic-desktop
+
+
 # install base packages
 dnf5 -y copr enable ublue-os/staging
 
-dnf5 install -y cosmic-desktop
+
 
 dnf5 -y copr disable ublue-os/staging
 
