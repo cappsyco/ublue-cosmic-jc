ebeea0e2bad42108b36917c0e1a91684eab39cf9
Jonathan Capps <cappsy@gmail.com>
2025-02-23 23:16:47 +0000

    fix package syntax

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ dnf5 -y copr enable ublue-os/staging
 file="/tmp/packages.txt"
 while read -r package; 
 do
-    if [ "$line" != "" ]; then
-        echo -e "dnf5 -y install $line\n"
+    if [ "$package" != "" ]; then
+        echo -e "dnf5 -y install $package\n"
     fi
 done <$file
 
