6c397d587a8a5b6d4777ce87bb83d1f1939a55b5
Jonathan Capps <cappsy@gmail.com>
2025-02-23 23:09:38 +0000

    refactoring package building

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 set -ouex pipefail
 
+
+## PACKAGES
+
 # enable copr
 dnf5 -y copr enable ublue-os/staging
 
+cp /ctx/packages.txt /tmp/packages.txt
 file="/tmp/packages.txt"
 while read -r package; 
 do
@@ -15,5 +19,15 @@ done <$file
 
 # disable copr
 dnf5 -y copr disable ublue-os/staging
+
+
+## FLATPAKS
+
+# install flatpaks
+
+# remove uneeded flatpaks from upstream
+
+
+## SERVICES
 
 # systemctl enable podman.socket
