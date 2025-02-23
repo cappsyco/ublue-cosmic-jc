aa2cf62cbb532aaa91cee64d4e6340153536e8ef
Jonathan Capps <cappsy@gmail.com>
2025-02-23 22:44:32 +0000

    refactor package installs

--- build.sh
+++ build.sh
@@ -2,23 +2,18 @@
 
 set -ouex pipefail
 
-### Install packages
+# enable copr
+dnf5 -y copr enable ublue-os/staging
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+file="packages.txt"
+while read -r package; 
+do
+    if [ "$line" != "" ]; then
+        echo -e "dnf5 -y install $line\n"
+    fi
+done <$file
 
-# this installs a package from fedora repos
-dnf install -y gh fastfetch
+# disable copr
+dnf5 -y copr disable ublue-os/staging
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# systemctl enable podman.socket
